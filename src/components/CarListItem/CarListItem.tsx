ed2ce26d3cf8a9e2776e872b65efa8710a293639
Izzet <izzetertas@gmail.com>
2019-07-09 23:19:31 +0100

    Development of CarListItem component

---
+++ src/components/CarListItem/CarListItem.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './CarListItem.scss'
+
+type CarListItemProps = {
+  loading?: boolean
+  stockNumber: number
+  manufacturerName: string
+  modelName: string
+  color: string
+  mileage: {
+    number: number
+    unit: string
+  }
+  fuelType: string
+  pictureUrl: string
+}
+
+export default function CarListItem(props: CarListItemProps) {
+  if(props.loading) {
+    return (
+      <div className='CarListItem-container'>
+        <div className='CarListItem-image'>
+        </div>
+        <div className='CarListItem-content'>
+          <div className='CarListItem-loading_long_and_height'>&nbsp;</div>
+          <div className='CarListItem-loading_long'>&nbsp;</div>
+          <div className='CarListItem-loading_short'>&nbsp;</div>
+        </div>
+      </div>
+    )
+  }
+
+  const getDetailInfo = () => {
+    return `Stock # ${props.stockNumber} - ${props.mileage.number} ${props.mileage.unit} - ${props.fuelType} - ${props.color}`
+  }
+
+  return (
+    <div className='CarListItem-container'>
+      <div className='CarListItem-image'>
+        <img src={props.pictureUrl} alt={props.modelName}/>
+      </div>
+      <div className='CarListItem-content'>
+        <div className='CarListItem-content__header'>{props.manufacturerName} {props.modelName}</div>
+        <div>{getDetailInfo()}</div>
+        <div className='CarListItem-details'>
+          <Link to={`/car/${props.stockNumber}`}>View details</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
