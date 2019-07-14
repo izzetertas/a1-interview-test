ae41176d9ccf1ebb19513fdff1bcf60c880734a4
Izzet <izzetertas@gmail.com>
2019-07-14 12:57:16 +0100

    CarListItem refactoring

--- src/components/CarListItem/CarListItem.tsx
+++ src/components/CarListItem/CarListItem.tsx
@@ -16,37 +16,38 @@ type CarListItemProps = {
   pictureUrl: string
 }
 
-export default function CarListItem(props: CarListItemProps) {
-  if(props.loading) {
+export default class CarListItem extends React.PureComponent<CarListItemProps> {
+  getDetailInfo = () => {
+    return `Stock # ${this.props.stockNumber} - ${this.props.mileage.number} ${this.props.mileage.unit} - ${this.props.fuelType} - ${this.props.color}`
+  }
+
+  render() {
+    if (this.props.loading) {
+      return (
+        <div className='CarListItem-container'>
+          <div className='CarListItem-image'>
+          </div>
+          <div className='CarListItem-content'>
+            <div className='CarListItem-loading_long_and_height'>&nbsp;</div>
+            <div className='CarListItem-loading_long'>&nbsp;</div>
+            <div className='CarListItem-loading_short'>&nbsp;</div>
+          </div>
+        </div>
+      )
+    }
     return (
       <div className='CarListItem-container'>
         <div className='CarListItem-image'>
+          <img src={this.props.pictureUrl} />
         </div>
         <div className='CarListItem-content'>
-          <div className='CarListItem-loading_long_and_height'>&nbsp;</div>
-          <div className='CarListItem-loading_long'>&nbsp;</div>
-          <div className='CarListItem-loading_short'>&nbsp;</div>
+          <div className='CarListItem-content__header'>{this.props.manufacturerName} {this.props.modelName}</div>
+          <div>{this.getDetailInfo()}</div>
+          <div className='CarListItem-details'>
+            <Link to={`/car/${this.props.stockNumber}`}>View details</Link>
+          </div>
         </div>
       </div>
     )
   }
-
-  const getDetailInfo = () => {
-    return `Stock # ${props.stockNumber} - ${props.mileage.number} ${props.mileage.unit} - ${props.fuelType} - ${props.color}`
-  }
-
-  return (
-    <div className='CarListItem-container'>
-      <div className='CarListItem-image'>
-        <img src={props.pictureUrl} />
-      </div>
-      <div className='CarListItem-content'>
-        <div className='CarListItem-content__header'>{props.manufacturerName} {props.modelName}</div>
-        <div>{getDetailInfo()}</div>
-        <div className='CarListItem-details'>
-          <Link to={`/car/${props.stockNumber}`}>View details</Link>
-        </div>
-      </div>
-    </div>
-  )
 }
