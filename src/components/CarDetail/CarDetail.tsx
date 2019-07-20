bab25d7e8e35c587fa196c666fc69d4fa773e031
Izzet <izzetertas@gmail.com>
2019-07-20 14:01:23 +0100

    CarDetail css improvement

--- src/components/CarDetail/CarDetail.tsx
+++ src/components/CarDetail/CarDetail.tsx
@@ -5,6 +5,9 @@ import axios from 'axios';
 import { AppState } from '../../../store/store';
 import { connect } from 'react-redux';
 import { Car } from '../../../store/types';
+import Button from '.././Button'
+
+import './CarDetail.scss';
 
 interface CarDetailComponentProps {
   match: {
@@ -28,18 +31,31 @@ function CarDetailComponent(props: CarDetailProps) {
       const result = await axios(`/api/cars/${props.match.params.stockNumber}`)
       setCarDetail(result.data.car)
     }
-    if(!carDetail) {
+    if (!carDetail) {
       fetchCarDetail()
     }
   })
 
-  if(!carDetail) return(<div>Car not found</div>)
-
+  if (!carDetail) return (<div>Car not found</div>)
   return (
-    <div>
-      CarDetail
-      <div>{carDetail.color}</div>
-        <Link to='/cars'>go to search page</Link>
+    <div className='CarDetail-container'>
+      <div className='CarDetail-img'></div>
+      <div className='CarDetail-content'>
+        <div className='CarDetail-detail'>
+          <h1>{carDetail.manufacturerName} {carDetail.modelName}</h1>
+          <h3>{`Stock # ${carDetail.stockNumber} - ${carDetail.mileage.number} ${carDetail.mileage.unit} - ${carDetail.fuelType} - ${carDetail.color}`}</h3>
+          <p>
+            This car is currently available and can ve delivered as soon as tomorrow morning.
+            Please be aware that delivery times shown in this page are not definitive and may change due to bad weather conditions.
+        </p>
+        </div>
+        <div className='CarDetail-favourite'>
+          <p>If you like this car, click the button and save it in your collection of favourite items.</p>
+          <div style={{ textAlign: 'right' }}>
+            <Button text='Save' />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
