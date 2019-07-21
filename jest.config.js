e1599386f10a042c99ac8fff3b9d27a8060f8753
Izzet <izzetertas@gmail.com>
2019-07-21 17:43:51 +0100

    ts-jest implementation

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  preset: 'ts-jest'
-};
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest",
+    ".+\\.scss$": "jest-css-modules-transform",
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+  testPathIgnorePatterns: ["/node_modules/"],
+  "moduleNameMapper": {
+    ".+\\.scss$": require.resolve("./__mocks__/styleMock.js"),
+  },
+  "transformIgnorePatterns" : [
+    "^.d.ts?$"
+  ],
+}
